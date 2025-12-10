808df3db9b775ab3ef175f32956ae6e3ffe7e693
Advait Tilak <advaittilak@gmail.com>
2025-12-10 14:28:35 -0600

    Solve error.

--- dashboard.py
+++ dashboard.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
-from matplotlib.pyplot import annotate
+
 
 # --- PAGE CONFIGURATION ---
 st.set_page_config(
